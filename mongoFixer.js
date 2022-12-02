5ea0c2391f90e31759e24af870bb310a4c62ca33
Jonas <64926407+jqlifice@users.noreply.github.com>
2022-12-02 09:28:37 +0100

    Add error for invalid file
    Now throws an error if provided with a file that can not be read.

--- mongoFixer.js
+++ mongoFixer.js
@@ -1,21 +1,25 @@
 let fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
-	let regex = new RegExp(`/${process.argv[3]}/g`);
-	let result = data.replace(regex, `"_id":`);
-	let newFileTemp = process.argv[2].split(".json");
-	let newFileName = newFileTemp[0]+"_fixed.json";
-	fs.open(newFileName, 'a', function(err, fd){
-		if(err){
-			console.log("write failed: "+err);
-		}else{
-			fs.write(fd, result, (err, bytes) => {
-				if(err){
-					console.log("write failed: "+err);
-				}else{
-					console.log("success");
-				}
-			}); 
-		}
-	});
+	if(err) {
+		console.log("could not read input file '"+process.argv[2]+"' (err: "+err+")");	
+	}else{
+		let regex = new RegExp(`/${process.argv[3]}/g`);
+		let result = data.replace(regex, `"_id":`);
+		let newFileTemp = process.argv[2].split(".json");
+		let newFileName = newFileTemp[0]+"_fixed.json";
+		fs.open(newFileName, 'a', function(err, fd){
+			if(err){
+				console.log("write failed: "+err);
+			}else{
+				fs.write(fd, result, (err, bytes) => {
+					if(err){
+						console.log("write failed: "+err);
+					}else{
+						console.log("success");
+					}
+				}); 
+			}
+		});	
+	}
 });
